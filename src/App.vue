4849f1a214dce80e5b9982494ac1105a1967e157
Administrateur <admin@sourcinghub.com>
2026-08-12 21:12:52 +0000

    chore: configuration de l'application racine et point d'entree

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+/* Les styles de base Tailwind sont dans main.css */
+</style>
