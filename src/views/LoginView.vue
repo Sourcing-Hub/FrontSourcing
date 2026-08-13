ab4f4def921bca437d6c1389989903903bf3d0ae
Administrateur <admin@sourcinghub.com>
2026-08-13 16:53:15 +0000

    style: ajout du fond avec motif d icones education (chapeau de diplome, ordi, code, cahier)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -76,9 +76,9 @@ const handleResetPassword = async () => {
       </div>
     </div>
 
-    <!-- Section Formulaire avec fond sombre #00313C, motif de cubes et Blobs animés (Droite) -->
+    <!-- Section Formulaire avec fond clair et motif d'icônes Éducation/Tech (Droite) -->
     <div
-      class="flex-1 flex items-center justify-center bg-[#00313C] bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] bg-repeat py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden"
+      class="flex-1 flex items-center justify-center bg-slate-50 bg-[url('/images/bg-pattern.svg')] bg-repeat py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden"
     >
       <!-- Blobs animés en arrière-plan -->
       <div
